0a8b472aaf799e26c6a3c4730e0631368283e250
jfs-code <jflorez367@misena.edu.co>
2024-06-22 12:50:57 -0500

    Organización de carpetas y rutas

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect  } from 'react';
 import PropTypes from 'prop-types';
-import "./navbar.css";
+import "./css/Navbar.css";
 
 export function Navbar({ brand, links }) {
     const [active, setActive] = useState('nav__menu');
